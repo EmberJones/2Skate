f76f9569560034464b5d781e384f4274db2d72d6
NemuruCodes <166004377+NemuruCodes@users.noreply.github.com>
2026-09-16 14:48:57 +0200

    Items

--- Assets/Scripts/SkateItem.cs
+++ Assets/Scripts/SkateItem.cs
@@ -6,8 +6,16 @@ public class SkateItem : CarryableItem
 {
     public bool isReturned = false;
     public static List<SkateItem> Available = new List<SkateItem>();
+    Vector3 _spawnPosition;
+    Quaternion _spawnRotation;
 
     public override bool CanBePickedUp => !isReturned;
+
+    void Awake()
+    {
+        _spawnPosition = transform.position;
+        _spawnRotation = transform.rotation;
+    }
 
     void OnEnable()
     {
@@ -19,6 +27,13 @@ public class SkateItem : CarryableItem
         {
             if (outline != null) outline.enabled = false;
         }
+    }
+    public override void OnDropped()
+    {
+        // Reappear back at its rack so it's always findable again.
+        transform.SetPositionAndRotation(_spawnPosition, _spawnRotation);
+        gameObject.SetActive(true);
+        outline.enabled = true;
     }
 
     void OnDisable()
